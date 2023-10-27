20b01baf271da0f1ea7a4baa03a64cb3eee8672f
Tanya <Tanja_33@mail.ru>
2023-10-27 13:17:05 +0700

    add test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,51 @@
 package ru.netology.service;
 
+import org.testng.Assert;
+
 public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testFinalBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testBuyOneHundredMore() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void testBuyOneMore() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testBuyOneHundredAndOne() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 899;
+
+        int actual = service.remain(amount);
+        int expected = 101;
+
+        Assert.assertEquals(actual, expected);
+    }
 }
